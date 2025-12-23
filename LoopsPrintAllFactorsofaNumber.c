726fa24f5a6b651db8faabca72c356cc39e2ea7b
Aryan Gupta <aryann.gupta.1ii@gmail.com>
2025-12-23 23:54:22 +0530

    Factor printing for a given number
    Added code to print all factors of a number in ascending order.

---
+++ LoopsPrintAllFactorsofaNumber.c
@@ -0,0 +1,65 @@
+//Problem
+
+/*Given a positive integer N, print all its factors in ascending order.
+
+You need to write a program that reads N and prints all its factors separated by spaces.
+
+Input Format
+
+A single line containing the integer N.
+
+Constraints
+
+1 ≤ 𝑁 ≤ 10^9
+Output Format
+
+Print all factors of N in ascending order, separated by a single space.
+
+Sample Input 0
+
+6
+Sample Output 0
+
+1 2 3 6
+Sample Input 1
+
+1
+Sample Output 1
+
+1
+Sample Input 2
+
+25
+Sample Output 2
+
+1 5 25*/
+
+//SOLUTION
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    long long N;
+    scanf("%lld", &N);
+
+    long long large[100000];
+    int k = 0;
+
+    for (long long i = 1; i * i <= N; i++) {
+        if (N % i == 0) {
+            printf("%lld ", i);
+            if (i != N / i) {
+                large[k++] = N / i;
+            }
+        }
+    }
+
+    for (int i = k - 1; i >= 0; i--) {
+        printf("%lld ", large[i]);
+    }    
+    return 0;
+}
