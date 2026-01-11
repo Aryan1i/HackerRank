e49f7e553d13d778440dbc1caf223e3cc5a09998
Aryan Gupta <aryann.gupta.1ii@gmail.com>
2026-01-12 00:47:19 +0530

    Find Max And Min

---
+++ Array/FindMaxAndMin.c
@@ -0,0 +1,59 @@
+//Problem
+
+    /*Given an array of N integers, your task is to determine the maximum and minimum values in the array.
+    
+    Input Format
+    
+    The first line contains an integer N, the number of elements in the array.
+    The second line contains N space-separated integers A[i], representing the elements of the array.
+    Constraints
+    
+    1 ≤ N ≤ 10^5
+    −10^9 ≤ A[i] ≤ 10^9
+    Output Format
+    
+    Print two space-separated integers: the maximum and minimum values in the array.
+    Sample Input 0
+    
+    5  
+    1 3 5 7 9
+    Sample Output 0
+    
+    9 1
+    Sample Input 1
+    
+    4  
+    -5 -2 -9 -1
+    Sample Output 1
+    
+    -1 -9*/
+
+//Solution
+
+#include <stdio.h>
+
+int main() {
+
+    int size;
+    scanf("%d",&size);
+    
+    int arr[size];
+    for(int i=0;i<size;i++){
+        scanf("%d",&arr[i]);
+    }
+    
+    int min=arr[0];
+    int max=arr[0];
+    
+    for(int i=1;i<size;i++){
+        if(arr[i]<min){
+            min=arr[i];
+        }
+        if(arr[i]>max){
+            max=arr[i];
+        }
+    }
+    
+    printf("%d %d",max,min);
+    return 0;
+}
